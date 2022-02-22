7e34fb36df1c97509b11ac36f9aeeddb7b88c81e
victor-m-p <victormoeller@gmail.com>
2022-02-22 19:21:22 +0100

    major preprocessing clean-up and bug-fixing

--- MAG/preprocessing/psychology_check_files.py
+++ MAG/preprocessing/psychology_check_files.py
@@ -1,20 +1,35 @@
+'''
+VMP 2022-02-21: 
+just for checking files, does not do anything. 
+'''
 import pandas as pd 
 import matplotlib.pyplot as plt
 import numpy as np
 import datetime, time
+from tqdm import tqdm
 pd.options.mode.chained_assignment = None  # default='warn'
 
 # read stuff 
-psych_paa = pd.read_csv("/work/50114/MAG/data/psychology_paper_author.csv")
-psych_meta = pd.read_csv("/work/50114/MAG/data/psychology_paper_meta.csv")
-psych_ref = pd.read_csv("/work/50114/MAG/data/psychology_reference.csv")
+psych_paa = pd.read_csv("/work/50114/MAG/data/MAG/psychology_paper_author.csv")
+psych_meta = pd.read_csv("/work/50114/MAG/data/MAG/psychology_paper_meta.csv")
+psych_ref = pd.read_csv("/work/50114/MAG/data/MAG/psychology_reference.csv")
 
-# fix date format
-psych_meta["Date"] = pd.to_datetime(psych_meta["Date"]).dt.date
+# check merge here (left vs. inner) -- not the issue
+def check_teamsize_join(df_paa, df_meta, type):
+    '''
+    type: <str> inner or left 
+    '''
+    # do the merging
+    paper_teamsize = df_paa.groupby('PaperId').size().reset_index(name='n_authors')
+    df_meta_teamsize = df_meta.merge(paper_teamsize, on = 'PaperId', how = type)
 
-# number of unique papers 
-len(psych_paa['PaperId'].unique()) # ~4.880.189
-len(psych_meta['PaperId'].unique()) # ~2.138.960
+    # print information
+    divisor = 1000000
+    print(f"{type}: {round(len(df_meta_teamsize)/divisor, 5)} million")
 
+check_teamsize_join(psych_paa, psych_meta, "left")
+check_teamsize_join(psych_paa, psych_meta, "inner")
 
-psych_paa.head(2)
+# check how many papers are published in several fields of study
+papers_duplicate = psych_meta.groupby('PaperId').size().reset_index(name = 'N').sort_values('N', ascending=False)
+papers_duplicate.groupby('N').size() # very few duplications
